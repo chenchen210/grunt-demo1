68cf8b78adb9b45caf2427284eecb38d17cd1a2c
chenchen210 <1486001914@qq.com>
2019-03-13 17:11:48 +0800

    htmlhint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,16 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-      htmlhint: {
-        html: {
-          options: {
-              htmlhintrc: '.htmlhintrc'   
-          },
-            src: ['*.html']            
-        }                  
-      }
-    });
-grunt.loadNpmTasks('grunt-htmlhint');
+  grunt.initConfig({
+    htmlhint: {
+      html: {
+        options: {
+          htmlhintrc: '.htmlhintrc'   
+        },
+        src: ['*.html']            
+      }                  
+    }
+  });
+  grunt.loadNpmTasks('grunt-htmlhint');
 
-grunt.registerTask('default', ['htmlhint']);
+  grunt.registerTask('default', ['htmlhint']);
 
 };
